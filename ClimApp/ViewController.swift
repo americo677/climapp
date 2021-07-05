a58d98cc303edef63c2bd285151b52ca72fa6e4f
Américo Cantillo <americo677@gmail.com>
2021-07-04 21:48:43 -0500

    Initial Commit

---
+++ ClimApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClimApp
+//
+//  Created by Américo Cantillo Gutiérrez on 4/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
